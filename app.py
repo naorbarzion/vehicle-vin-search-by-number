e7db964fc02fd009809eea3a4db1be5a059df61e
naorbarzion <149252537+naorbarzion@users.noreply.github.com>
2024-09-08 20:19:03 +0300

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import requests
 
@@ -34,4 +35,5 @@ def index():
     return render_template('index.html', records=records)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Listen on the appropriate port
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
